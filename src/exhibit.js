03de6e568ce75a0e4d96192a5010706a0c8d24ad
Stacey McKnight <mcknight.stm@gmail.com>
2021-04-01 15:50:16 -0500

    Change exhibit card content on click

--- src/exhibit.js
+++ src/exhibit.js
@@ -10,29 +10,54 @@ class Exhibit {
     this.zoneId = zone_id;
     this.imageLink = exhibit_image["cloudinary"];
     this.fact = species_fact;
+
     this.element = document.createElement("div");
+    this.element.classList.add("exhibit-card");
+    this.element.id = `exhibit-${id}`
+
+    this.element.addEventListener("click", this.handleClick);
   }
 
-  renderFront() {
+  render() { //figure out alt text for already loaded images and user submitted images 
+    //create image
+    const img = document.createElement("img");
+    img.src = this.imageLink;
+    
+    //info - front
     const h4 = document.createElement("h4");
     const p = document.createElement("p");
-    const img = document.createElement("img");
     
-    this.element.classList.add("exhibit-card");
-
     h4.innerText = this.name;
     p.innerText = this.description;
-    img.src = this.imageLink;
-    //figure out alt text for already loaded images and user submitted images 
 
+    //build card
     this.element.appendChild(img);
     this.element.appendChild(h4);
     this.element.appendChild(p);
+    this.element.classList.add("front");
   }
 
   attachToDom() {
     this.element.innerHTML = "";
-    this.renderFront();
+    this.render();
     Exhibit.exhibitContainer.appendChild(this.element);
+  }
+
+  handleClick = (e) => {
+    const card = this.element;
+
+    if(card.classList.contains("front")) {
+      card.classList.remove("front");
+      card.classList.add("back");
+
+      card.querySelector("h4").innerText = "Did you know?";
+      card.querySelector("p").innerText = this.fact;
+    }
+    else {
+      card.classList.remove("back");
+      card.classList.add("front");
+      card.querySelector("h4").innerText = this.name;
+      card.querySelector("p").innerText = this.description;
+    }
   }
 }
