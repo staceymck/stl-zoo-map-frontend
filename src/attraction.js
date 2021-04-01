9cc13c87e5eac9cb4c30079853bf825be1d43594
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-31 19:29:07 -0500

    Add attraction class

---
+++ src/attraction.js
@@ -0,0 +1,18 @@
+class Attraction {
+  
+  static all = []
+
+  constructor({id, name, zone_id}) {
+    this.id = id;
+    this.name = name;
+    this.zone_id = zone_id;
+
+    Attraction.all.push(this)
+  }
+
+  renderAsLi() {
+    const li = document.createElement("li");
+    li.innerText = this.name;
+    return li;
+  }
+}
