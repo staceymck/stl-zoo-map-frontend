3d2442561d2b3131a50c9788f9058634ae63c354
Stacey McKnight <mcknight.stm@gmail.com>
2021-04-02 15:43:39 -0500

    Add way to scroll from bottom of page to top on button click

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ logo.addEventListener('click', e => {
   document.querySelector("#exhibits h3").style.display = "none";
   document.querySelector("#exhibit-card-container").innerHTML = "";
 })
+
+//Scroll to top button
+document.querySelector("#top").addEventListener("click", (e) => {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  }) //how to make the focus happen after the scroll?
+  document.querySelector("#site-name").focus();
+})
