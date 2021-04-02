c480a261f6ba53d7437d676f1c5e8205b820c214
Stacey McKnight <mcknight.stm@gmail.com>
2021-04-02 09:41:38 -0500

    Change background color of zone info section for each zone

--- src/zone.js
+++ src/zone.js
@@ -7,8 +7,8 @@ class Zone {
     this.name = name;
     this.description = description;
 
-    const nameAsId = this.name.replace(/'/g, "").replace(/ /g, '-').toLowerCase();
-    this.mapElement = document.querySelector(`#${nameAsId}`);
+    this.nameAsId = this.name.replace(/'/g, "").replace(/ /g, '-').toLowerCase();
+    this.mapElement = document.querySelector(`#${this.nameAsId}`);
     this.mapElement.dataset["id"] = id;
 
     Zone.all.push(this);
@@ -18,7 +18,7 @@ class Zone {
     Zone.container.parentElement.classList.add("zone-selected");
     Zone.container.innerHTML = "";
 
-    //Zone.container.style.backgroundColor = ;
+    Zone.container.style.backgroundColor = `var(--${this.nameAsId}-primary)`
 
     const name = document.createElement("h1");
     name.innerText = this.name;
@@ -28,12 +28,13 @@ class Zone {
     desc.innerText = this.description;
     Zone.container.appendChild(desc);
 
-    const attractions = Attraction.all.filter(att => parseInt(att.zoneId, 10) === this.id)
-    if (attractions.length !== 0) {
-      this.renderAttractionList(attractions);
+    //const attractions = Attraction.all.filter(att => parseInt(att.zoneId, 10) === this.id)
+    if (this.attractions().length !== 0) {
+      this.renderAttractionList(this.attractions());
     }
   }
 
+  //maybe move some of this to attractions class?
   renderAttractionList(attractions) {
     const header = document.createElement("h2");
     const list = document.createElement("ul");
@@ -46,5 +47,9 @@ class Zone {
 
     Zone.container.appendChild(header);
     Zone.container.appendChild(list);
+  }
+
+  attractions() {
+    return Attraction.all.filter(att => parseInt(att.zoneId, 10) === this.id)
   }
 }
