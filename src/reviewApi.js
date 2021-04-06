fac857cacce0711dfb30b92c08de303ef0ca92ac
Stacey McKnight <mcknight.stm@gmail.com>
2021-04-06 10:22:01 -0500

    Namespace formData obj to fit strong params

--- src/reviewApi.js
+++ src/reviewApi.js
@@ -1,7 +1,5 @@
 class ReviewApi {
 
-  static all = {}
-
   constructor(port) {
     this.baseUrl = `${port}/reviews`
   }
@@ -18,5 +16,35 @@ class ReviewApi {
   }
 
   createReview = () => {
+    //const formData = new FormData(e.target);
+    const imageFile = document.querySelector("#review-image").files[0];
+    
+    const formData = new FormData();
+    formData.append('review[username]', document.querySelector("#review-username").value);
+    formData.append('review[rating]', document.querySelector("#review-rating").value);
+    formData.append('review[content]', document.querySelector("#review-content").value);
+    formData.append('review[image]', imageFile, imageFile.name); 
+
+    console.log(formData);
+
+    const configObj = {
+      method: "POST",
+      headers: {
+        //no content-type for FormData
+        "Accept": "application/json" 
+      },
+      body: formData
+    }
+
+    fetch(this.baseUrl, configObj)
+    .then(res => res.json())
+    .then(data => {
+      new Review(data)
+      //would I need to re-render the reviews page?
+    })
+    .catch(error => {
+      alert("Unable to process request");
+      console.log(error.message);
+    })
   }
 }
